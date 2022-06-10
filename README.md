cc6e8d49c53f3b1f8810f8384c6d268904807ce8
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-06-10 18:53:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicios_js
+ Exercicios de JavaScript
---
+++ horario_site/script.js
@@ -0,0 +1,18 @@
+var tempo = new Date()
+//var hora = tempo.getHours()
+var hora = 19
+ function carregar(){
+    var msg = document.querySelector('div#msg')
+    var img = document.querySelector('img#imagem')
+    var fundo = document.querySelector('body#fundo')
+    msg.innerHTML = `Agora são exatamente ${hora}h`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.jpg'
+    }else if (hora >=12 && hora < 18){
+        img.src = 'tarde.jpg'
+        fundo.style.background = 'orange'
+    }else{
+        img.src = 'noite.jpg'
+        fundo.style.background = 'black'
+    }
+ }
